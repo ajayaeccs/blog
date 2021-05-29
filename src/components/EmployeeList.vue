5a4c8ccb2097bc238d6526104d8db218c3888cea
ajay <ajayaeccs@gmail.com>
2021-05-29 20:06:16 +0530

    Example to fetch data from axios dummy api

---
+++ src/components/EmployeeList.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <table border="2px" class="employee">
+        <caption>Employees List</caption>
+        <tr>
+            <th>Employee ID</th>
+            <th>Employee Name</th>
+            <th>Employee Salary</th>
+            <th>Employee Age</th>
+        </tr>
+        <tr v-for="e in employeeList" v-bind:key="e.id">
+            <td>{{e.id}}</td>
+            <td>{{e.employee_name}}</td>
+            <td>{{e.employee_salary}}</td>
+            <td>{{e.employee_age}}</td>
+        </tr>
+    </table>
+  </div>
+</template>
+        
+       
+<script>
+import Vue from 'vue' 
+import VueAxios from 'vue-axios'
+import axios from 'axios'
+
+Vue.use(VueAxios, axios)
+
+export default {
+  name: "EmployeeList",
+  data(){
+      return{employeeList:[]}
+  },
+  mounted(){
+      Vue.axios.get('http://dummy.restapiexample.com/api/v1/employees')
+      .then(resp => {this.employeeList=resp.data.data})
+  },
+};
+</script>
+
+<style scoped>
+div{
+    background-color:antiquewhite;
+    height: 800px;
+}
+
+.employee{
+    border:2px solid blue;
+    border-collapse: collapse;
+    margin:30px;
+}
+</style>
